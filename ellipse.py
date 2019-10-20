104791b4c983c216ec37661ef9f4d8d305fc6909
Kudlur, M <43883751+571502680@users.noreply.github.com>
2019-10-20 17:17:59 +0800

    exercise
    exercise

--- ellipse.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-img = np.zeros((512,512,3),np.uint8)
-cv2.ellipse(img,(260,240),(170,290),0,0,360,(255,255,255),3)
-cv2.imshow("test",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
